e9cb600c19b3e0f17d9781a1f14d262a60c4100b
vysery98 <luispsi98@gmail.com>
2019-04-25 12:12:49 -0500

    Seleccion Anidada ejercicio

---
+++ Ejercicios/src/ejercicios/SeleccionAnidada.java
@@ -0,0 +1,35 @@
+package ejercicios;
+
+import java.util.Scanner;
+import paquetedos.OperacionDos;
+
+/**
+ *
+ * @author vysery98
+ */
+public class SeleccionAnidada {
+
+    public static void main(String[] args) {
+        // documentacion
+        Scanner scan = new Scanner(System.in);
+        int calificacion = scan.nextInt();
+
+        if (calificacion >= 90) {
+            System.out.printf("%s %d\n", OperacionDos.mensaje, calificacion);
+        } else {
+            if (calificacion < 90 && calificacion >= 80) {
+                System.out.printf("%s %d\n", OperacionDos.mensaje2,
+                        calificacion);
+            } else {
+                if (calificacion < 80 && calificacion >= 50) {
+                    System.out.printf("%s %d\n", OperacionDos.mensaje3,
+                            calificacion);
+                } else {
+                    System.out.printf("%s %d\n", OperacionDos.mensaje4,
+                            calificacion);
+                }
+            }
+        }
+
+    }
+}
---
+++ Ejercicios/src/paquetedos/OperacionDos.java
@@ -0,0 +1,13 @@
+package paquetedos;
+
+/**
+ *
+ * @author vysery98
+ */
+public class OperacionDos {
+
+    public static String mensaje = "Usted está aprobado (excelente) con ";
+    public static String mensaje2 = "Usted está aprobado (muy buena)con ";
+    public static String mensaje3 = "Usted está aprobado (regular)con ";
+    public static String mensaje4 = "Usted está reprobado con ";
+}
