e9b9291e8634193eb86ad34c0daa7afe85a42d49
vysery98 <luispsi98@gmail.com>
2019-04-25 11:33:13 -0500

    Seleccion Simple

---
+++ Ejercicios/src/ejercicios/SeleccionSimple.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+import paquetedos.Operacion;
+
+/**
+ *
+ * @author vysery98
+ */
+public class SeleccionSimple {
+
+    public static void main(String[] args) {
+        // método principal
+
+        String miMensaje = Operacion.mensaje;
+        int calificacion = 85;
+
+        if (calificacion >= 85) {
+            System.out.printf("%s %d\n", miMensaje, calificacion);
+        }
+
+        int calificacion_2 = 45;
+
+        if (calificacion_2 >= 85) {
+            System.out.printf("%s %d\n", miMensaje, calificacion);
+        }
+
+    }
+}
---
+++ Ejercicios/src/paquetedos/Operacion.java
@@ -0,0 +1,12 @@
+package paquetedos;
+
+/**
+ *
+ * @author vysery98
+ */
+public class Operacion {
+
+    //declaración de variable estática
+    public static String mensaje = "Usted está aprobado con ";
+
+}
